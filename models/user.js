e6d82897f1280914c3701c628bc4bd9765d5f7d1
InsuLee <Beis@iinsuui-MacBookPro.local>
2021-12-15 20:59:59 +0900

    centralizationServer

--- models/user.js
+++ models/user.js
@@ -1,21 +1,26 @@
 'use strict';
-const { Model } = require('sequelize');
-
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class user extends Model {
-        static associate(models) {
-            models.user.hasMany(models);
-        }
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-    user.init({
-        userName: DataTypes.STRING,
-        password: DataTypes.STRING,
-        address: DataTypes.STRING,
-        privateKey: DataTypes.STRING
-    }, {
-        sequelize,
-        modelName: 'user'
-    });
-
-    return user;
-}
+  };
+  User.init({
+    userName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    address: DataTypes.STRING,
+    privateKey: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
--- routes/wallet/index.js
+++ routes/wallet/index.js
@@ -2,29 +2,82 @@ var express = require('express');
 var router = express.Router();
 const lightwallet = require("eth-lightwallet");
 const fs = require('fs');
-const { user } = require('../../models');
+const { User } = require('../../models');
 
 router.post('/user', async(req,res) => {
   // 포스트맨에서 userName, password를 넣으면
-  let userName, password, privateKey, address;
+  let reqUserName, reqPassword;
+  reqUserName = req.body.userName;
+  reqPassword = req.body.password;
 
-  // user에서 findOrCreate로 userName을 찾고,
-  user.findOrCreate({
+  // user에서 find로 userName을 찾고,
+  User.findOrCreate({
     where: {
-      userName: req.body.userName
+      userName: reqUserName
+    },
+    default: {
+      password: reqPassword
     }
   })
-    // 있으면 있다고 응답.
+  .then(([user, created]) => {
+    if (!created) {
+      // 있으면 있다고 응답.
+      res.status(409).send("User exists");
     // 없으면 DB에 저장
-      // 니모닉코드 생성 
+    } else {
+      // 니모닉코드 생성  
+      let mnemonic;
+      mnemonic = lightwallet.keystore.generateRandomSeed();
+      // console.log(mnemonic);
       // 생성된 니모닉코드와 password로 privateKey, address 생성
-      // DB에 저장
-    // .then 해서 보여준다. 주소를 보여준다.
-  console.log(req.body);
+      lightwallet.keystore.createVault({
+        password: reqPassword, 
+        seedPhrase: mnemonic,
+        hdPathString: "m/0'/0'/0'"
+      },
+      function (err, ks) {
+        ks.keyFromPassword(reqPassword, function (err, pwDerivedKey) {
+          ks.generateNewAddress(pwDerivedKey, 1);
+          
+          let address = (ks.getAddresses()).toString();
+          let keyStore = ks.serialize();
+
+          // 키스토어 저장
+          // fs.writeFile('wallet.json',keyStore,function(err,data){
+          //   if(err) {
+          //     res.json({code:999,message:"실패"});
+          //   } 
+          // })     
+
+          User.update({
+            password: reqPassword,
+            address: address,
+            privateKey: mnemonic
+          }, {
+            where: {
+              userName: reqUserName
+            }
+          })
+          .then(result => {
+            // 주소를 보여준다.
+            res.json(address);
+          })
+          .catch(err => {
+            console.error(err);
+          })
+        });
+      });
+      // const resp = user.get({ plain: true });
+      // res.status(201).json(resp);
+    }
+  })
 });
 
 
-
+// 개인키 생성은 키스토어.json을 추출함으로서 사용해야 함. (다른 요청)
+// wallet = fs.readFile('wallet.json');
+// newKeystore = lightwallet.keystore.deserialize(wallet);
+// let privateKey = newKeystore.exportPrivateKey(address.toString(), pwDerivedKey);
 
 
 // lightwallet 모듈을 사용하여 랜덤한 니모닉 코드를 얻습니다.
